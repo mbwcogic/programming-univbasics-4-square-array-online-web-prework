22da306f3e9334bcea29c42a856d43acdf7f1112
Michael Waldo <mbwcogic@yahoo.com>
2019-09-22 16:37:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,7 +9,14 @@ results << array[counter] **2
   
 end
 results
-end
+
+#1.) create a variable called results and set it equal to an empty array as the first line of the method
+User avatar
+Jonathan Foster 14 MINUTES AGO
+2.) loop through array, and for each time you go through, push the squared value of the array at that position into the results array
+User avatar
+Jonathan Foster 14 MINUTES AGO
+3.) after the loop is done, return the results array
  
 
 
