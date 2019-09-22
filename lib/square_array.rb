6246864e79c53c7fbb9364a33898ade80e61f6cd
Michael Waldo <mbwcogic@yahoo.com>
2019-09-22 16:43:44 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,22 @@
 def square_array(array)
-  # your code here
+  counter=0
+
+results = []
+ while array[counter] do
+results << array[counter] **2  
+  
+  counter += 1 
+  
 end
+results
+
+#1.) create a variable called results and set it equal to an empty array as the first line of the method
+User avatar
+Jonathan Foster 14 MINUTES AGO
+2.) loop through array, and for each time you go through, push the squared value of the array at that position into the results array
+User avatar
+Jonathan Foster 14 MINUTES AGO
+3.) after the loop is done, return the results array
+ 
+
+
